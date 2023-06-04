4188b702031837d01d8037b81b1d67c72229577e
habib24457 <habiburehman390@gmail.com>
2023-06-04 14:42:14 +0200

    added initial files

---
+++ MagicVilla_VillaAPI/Controllers/VanillaAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/VillaAPI")]
+	[ApiController]
+	public class VanillaAPIController : ControllerBase
+	{
+		//gets all the datas
+		[HttpGet]
+		public IEnumerable<VillaDTO>GetVillas()
+		{
+			return VillaStore.villaList;
+		}
+
+
+		[HttpGet("{id:int}")]
+		public VillaDTO GetOneVilla(int id)
+		{
+			return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		}
+	}
+}
+
